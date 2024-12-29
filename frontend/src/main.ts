783fe2d908e71769061ea7ac5bed3b98ab3ea180
maniak <maniak@wp.pl>
2024-12-29 12:11:01 +0100

    dodano NgxPiwikProModule

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,10 +7,12 @@ import { routes } from './app/app.routes';
 import { credentialsInterceptor } from './app/app/utils/credentials.interceptor';
 import { MatNativeDateModule } from '@angular/material/core';
 import { importProvidersFrom } from '@angular/core';
+import { NgxPiwikProModule } from '@piwikpro/ngx-piwik-pro';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(MatNativeDateModule),
+    importProvidersFrom(MatNativeDateModule, NgxPiwikProModule.forRoot('0d314679-75fb-4ce1-ac29-25f1e0661661', 'https://wpxu.piwik.pro')),
     provideHttpClient(
       withInterceptors([credentialsInterceptor])
     ),
